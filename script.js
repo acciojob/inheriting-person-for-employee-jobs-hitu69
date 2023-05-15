eec229407f19342ed936b47eee5af387eb155bd8
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-05-15 13:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	  this.name = name;
+  this.age = age;
 
-function Employee(name, age, jobTitle) {}
+}
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+
+
+function Employee(name, age, jobTitle) {
+	  Person.call(this, name, age); // call Person constructor function with 'this' context
+  this.jobTitle = jobTitle;
+
+}
+Employee.prototype = Object.create(Person.prototype);
+
+// Set constructor property back to Employee constructor function
+Employee.prototype.constructor = Employee;
+
+// Employee prototype method
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
+
 
 // Do not change code below this line
 window.Person = Person;
